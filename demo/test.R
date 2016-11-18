574659b32f777264801d1b4829d8eaa8361875bd
jeanmarielepioufle <jm@nexmodeling.com>
2016-11-18 11:38:24 +0100

    add install ddd in test

--- demo/test.R
+++ demo/test.R
@@ -1,5 +1,8 @@
 library(onionR)
 
+devtool::install_github("nexmodeling/ddd")
+library(ddd)
+
 mysnow <- block(namefield="snow",namespace="dddSnow",pathRes="~/")
 mysnow$do("init.snow",args=list(method="manual",isoil=0,gisoil=0,spd=0,wcd=0,sca=0,nsno=0,alfa=0,ny=0,snowfree=0))
 mysnow$save("test")
